f63667b54ecd00505da1c142a785aef6e65f3646
Sajol Das <sajoldas421@example.com>
2023-09-04 05:56:11 +0600

    comment

--- models/Comment.js
+++ models/Comment.js
@@ -6,19 +6,46 @@ const commentSchema = new Schema({
     task: {
         type: mongoose.Types.ObjectId,
         ref: 'task',
+        validate: {
+            validator: (value) => {
+                const result = Joi.string().regex(/^[0-9a-fA-F]{24}$/).required().validate(value)
+                (!result) ? false : true;
+            },
+            message: 'task validate error for sommentSchema'
+        }
     },
     author: {
         type: mongoose.Types.ObjectId,
         ref: 'User',
+        validate: {
+            validator: (value) => {
+                const result = Joi.string().regex(/^[0-9a-fA-F]{24}$/).required().validate(value);
+                (!result) ? false : true;
+            },
+            message: 'author validation errr for commentSchema'
+        }
     },
     text: {
         type: String,
         required: true,
+        validate: {
+            validator: (value) => {
+                const result = Joi.string().required().validate(value);
+                (!result) ? false : true;
+            },
+            message: 'provide valid text for commentSchema'
+        }
        
     },
     createdAt: {
         type: Date,
         default: Date.now,
+        validate: {
+            validator: (value) => {
+                const result = Joi.date().default(Date.now).validate(value);
+                (!result) ? false : true;
+            },
+        }
     },
      
 });
