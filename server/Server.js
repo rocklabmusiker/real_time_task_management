d3c10d3b266e328cd8b0b41169fc32143fa8a168
Sajol Das <sajoldas421@example.com>
2023-09-07 01:19:54 +0600

    ff

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,13 @@
+const http = require('http');
+const socketIo = require('socket.io');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
+const passportMiddleware = require('./middleware/passport');
+const passport = require('passport');
+
+//routes
 const userRoutes = require('./routes/users/users');
 const boardRoutes = require('./routes/board/board');
 const taskRoutes = require('./routes/tasks/task');
@@ -11,25 +17,24 @@ const milestoneRoutes = require('./routes/milestone/milestone')
 const boardInvitationRouter = require('./routes/boardInvitation/boardInvitation')
 const notificationRoutes = require('./routes/notification/notification');
 const attachmentRouter = require('./routes/attachment/attachment')
-const passportMiddleware = require('./middleware/passport');
-const passport = require('passport');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 require('dotenv').config();
-const app = express();
 const PORT = process.env.PORT || 3000 
-
-//parse JSON Request
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 //Database Connection
 connectDB();
-
-//passport middleware
+//middleware
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 passportMiddleware(passport);
 
-//Routes
+//add Routes
 app.use('/api/auth', authRoutes)
 app.use('/user',userRoutes)
 app.use('/board',boardRoutes)
@@ -39,9 +44,16 @@ app.use('/', commentRoutes)
 app.use('/invitation', boardInvitationRouter)
 app.use('/notification', notificationRoutes)
 app.use('/attachment', attachmentRouter)
-app.use(cors())
-app.use(express.json())
 
-app.listen(PORT, () => {
+//socket io
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    console.log('message received'+ message);
+
+    socket.broadcast.emit('message', message);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`)
 });
